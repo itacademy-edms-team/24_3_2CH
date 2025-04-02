39155d603f694b910710c77969938f75dc1c2edd
arbuz <166127879+GitHEXual@users.noreply.github.com>
2025-04-02 17:40:06 +0700

    Рефакторинг

---
+++ MyImageBoard/wwwroot/js/ImageViewer.js
@@ -0,0 +1,36 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('imageModal');
+    const modalImage = document.getElementById('modalImage');
+    const modalDialog = document.querySelector('#imageModal .modal-dialog');
+
+    modal.addEventListener('show.bs.modal', (event) => {
+        const imageUrl = event.relatedTarget.getAttribute('data-bs-image');
+        modalImage.src = imageUrl;
+
+        modalImage.onload = () => {
+            const imgWidth = modalImage.naturalWidth;
+            const imgHeight = modalImage.naturalHeight;
+            const maxWidth = window.innerWidth * 0.9;
+            const maxHeight = window.innerHeight * 0.9;
+
+            let newWidth = imgWidth;
+            let newHeight = imgHeight;
+            if (newWidth > maxWidth) {
+                newHeight = (maxWidth / newWidth) * newHeight;
+                newWidth = maxWidth;
+            }
+            if (newHeight > maxHeight) {
+                newWidth = (maxHeight / newHeight) * newWidth;
+                newHeight = maxHeight;
+            }
+
+            modalDialog.style.width = `${newWidth}px`;
+            modalDialog.style.height = `${newHeight}px`;
+        };
+    });
+
+    modal.addEventListener('hidden.bs.modal', () => {
+        modalDialog.style.width = '';
+        modalDialog.style.height = '';
+    });
+});
